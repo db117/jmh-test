c3a02841e741b3e7c438d2df7d4e3be78b3ca61b
db117 <z351622948@163.com>
2021-05-08 17:52:03 +0800

    string拼接测试

--- src/main/java/cn/db117/jmh/StringAppendJmhTest.java
+++ src/main/java/cn/db117/jmh/StringAppendJmhTest.java
@@ -32,6 +32,9 @@ public class StringAppendJmhTest {
     @Benchmark
     @Fork(1)
     public void add() {
+        // 2581.922 ±(99.9%) 446.054 ops/s [Average]
+        //  (min, avg, max) = (2469.446, 2581.922, 2726.632), stdev = 115.839
+        //  CI (99.9%): [2135.868, 3027.975] (assumes normal distribution)
         data.forEach((s, s2) -> {
             String res = s + s2;
         });
@@ -40,6 +43,9 @@ public class StringAppendJmhTest {
     @Benchmark
     @Fork(1)
     public void format() {
+        // 130.687 ±(99.9%) 3.448 ops/s [Average]
+        //  (min, avg, max) = (129.309, 130.687, 131.480), stdev = 0.895
+        //  CI (99.9%): [127.239, 134.136] (assumes normal distribution)
         data.forEach((s, s2) -> {
             String res = String.format("%s%s", s, s2);
         });
@@ -48,6 +54,9 @@ public class StringAppendJmhTest {
     @Benchmark
     @Fork(1)
     public void stringBuild() {
+        //   3292.527 ±(99.9%) 108.735 ops/s [Average]
+        //  (min, avg, max) = (3248.656, 3292.527, 3317.552), stdev = 28.238
+        //  CI (99.9%): [3183.791, 3401.262] (assumes normal distribution)
         data.forEach((s, s2) -> {
             String res = new StringBuilder().append(s).append(s2).toString();
         });
